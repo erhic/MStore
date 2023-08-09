f5a6a06f15b2260153c3ec5b32806d1bdc09ee41
erhic <ericngugi24@gmail.com>
2023-08-09 23:35:14 +0300

    added an interface with a json file to act as backend data

--- src/app/product.ts
+++ src/app/product.ts
@@ -1,8 +1,39 @@
 
-//Main purpose of the inteface is to enable strict typing of the listed variables
+//Main purpose of the inteface is to enable strict typing of the listed variables ,an interface is an object with keys and property
 export interface  Product{
   id: number,
   name:string,
   price:number,
   description:string
 }
+
+// a json file to act as a backend  data
+// a json file is an array that consists objects
+
+export const products = [
+  {
+  id: 1,
+  name:'PIhone',
+  price:488,
+  description:'A large phone with one of the best screens'
+},
+{
+  id: 2,
+  name:'Huawei',
+  price:600,
+  description:'A extra large phone with one of the best screens'
+},
+{
+  id: 3,
+  name:'Samsung',
+  price:444,
+  description:'A medium phone with one of the best screens'
+},
+{
+  id: 4,
+  name:'Nokia',
+  price:700,
+  description:'A small phone with one of the best screens'
+}
+
+];
