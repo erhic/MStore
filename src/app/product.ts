a28a179ae91eb2ea92ab9ab8e5a7a88954c0f32f
erhic <ericngugi24@gmail.com>
2023-08-09 23:28:38 +0300

    added an interface

---
+++ src/app/product.ts
@@ -0,0 +1,8 @@
+
+//Main purpose of the inteface is to enable strict typing of the listed variables
+export interface  Product{
+  id: number,
+  name:string,
+  price:number,
+  description:string
+}
