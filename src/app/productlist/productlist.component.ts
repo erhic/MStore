d7c7c7cb421b35e8d934904b5b276aede3183274
erhic <ericngugi24@gmail.com>
2023-08-10 00:44:38 +0300

    added a function logic to alert on every button click in the products list

--- src/app/productlist/productlist.component.ts
+++ src/app/productlist/productlist.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import {  products } from '../product';
+import { Component, OnInit,Input } from '@angular/core';
+import {  products ,  Product } from '../product';
 
 @Component({
   selector: 'app-productlist',
@@ -10,9 +10,10 @@ export class ProductlistComponent  {
 //we are storing the data from the json file here to be looped in the template of theis component
   products = [...products]
   
+  // @Input() product=  Product |undefined
 
   //a function that is called by click of share button in template of this component
   share(){
-
+window.alert('Product shared successfully' )
   }
 }
