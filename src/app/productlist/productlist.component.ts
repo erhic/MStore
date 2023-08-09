ea6541e8872fabe07cd5367d4fe3d685b925267c
erhic <ericngugi24@gmail.com>
2023-08-10 00:23:39 +0300

    added list of items from the product list componentrs by making loops on the imported data from interface to the component and the in the template

--- src/app/productlist/productlist.component.ts
+++ src/app/productlist/productlist.component.ts
@@ -1,16 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from '../product';
+import {  products } from '../product';
 
 @Component({
   selector: 'app-productlist',
   templateUrl: './productlist.component.html',
   styleUrls: ['./productlist.component.css']
 })
-export class ProductlistComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class ProductlistComponent  {
+//we are storing the data from the json file here to be looped in the template of theis component
+  products = [...products]
+  
 }
