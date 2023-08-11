a7a2ebceac8d95f77f2659c542038e1aa42f1cec
erhic <ericngugi24@gmail.com>
2023-08-12 00:03:56 +0300

    added router path

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,23 @@ import { AppComponent } from './app.component';
 import { ProductlistComponent } from './productlist/productlist.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductlistComponent,
     TopBarComponent,
-    ProductAlertsComponent
+    ProductAlertsComponent,
+    ProductDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,ReactiveFormsModule,RouterModule.forRoot([
+      {path:'',component:ProductlistComponent},
+      {path:'products/productId',component:ProductDetailsComponent},
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
