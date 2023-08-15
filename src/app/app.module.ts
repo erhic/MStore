4f8833a06c7ed9ede6920f3d7c6954b91ad51fba
erhic <ericngugi24@gmail.com>
2023-08-15 15:13:39 +0300

    added a method in service to observe data in the json file , to make calls in request of shipping of info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { ProductlistComponent } from './productlist/productlist.component';
@@ -27,7 +28,7 @@ const routes= [
     CartComponent
   ],
   imports: [
-    BrowserModule,ReactiveFormsModule, RouterModule.forRoot(routes)
+    BrowserModule,ReactiveFormsModule,HttpClientModule, RouterModule.forRoot(routes),
   ],
   providers: [CartService],
   bootstrap: [AppComponent]
--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Product } from './product';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
 items:Product[]=[]
-  constructor() { }
+  constructor(private http:HttpClient) { }
 
   addToCart(product:Product){
 this.items.push(product)
@@ -17,5 +18,8 @@ return this.items
   clearCart(){
 this.items=[];
 return this.items;
+  }
+  getShippingPrice(){
+    return this.http.get<{type:string,price:number}[]>('/assets/shipping.json')
   }
 }
